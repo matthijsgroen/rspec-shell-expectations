baf401da1535a441797cb900d184d712697fbb39
Ben Brewer <ben@benbrewer.me>
2016-08-06 13:38:23 -0400

    moves be_called_with_arg matcher tests into new spec file
    - makes tests slightly more expressive
    - makes way for addition of a new, better matcher that will deprecate it

--- spec/assert_called_spec.rb
+++ spec/assert_called_spec.rb
@@ -1,20 +1,14 @@
 require 'English'
 require 'rspec/shell/expectations'
-require 'rspec/shell/expectations/matchers/called_with_arg.rb'
 
 describe 'Assert called' do
   include Rspec::Shell::Expectations
   let(:stubbed_env) { create_stubbed_env }
   let!(:first_command) { stubbed_env.stub_command('first_command') }
-  let!(:second_command) { stubbed_env.stub_command('second_command') }
-  let!(:flagged_command_stub) { stubbed_env.stub_command('flagged_command') }
 
   let(:script) do
     <<-SCRIPT
       first_command "foo bar"
-      second_command foo bar
-      second_command foo boo
-      flagged_command -d flagged_arg
     SCRIPT
   end
   let(:script_path) { Pathname.new '/tmp/test_script.sh' }
@@ -43,34 +37,6 @@ describe 'Assert called' do
         subject
         expect(first_command.with_args('foo bar')).to be_called
         expect(first_command.with_args('foo')).not_to be_called
-      end
-    end
-
-    context 'with arg check' do
-      it 'matches against entire set of arguments' do
-        subject
-        expect(second_command).to be_called_with_arg('foo')
-        expect(second_command).to be_called_with_arg('bar')
-        expect(second_command).to_not be_called_with_arg('baz')
-      end
-    end
-
-    context 'with arg at position' do
-      it 'matches against argument position' do
-        subject
-        expect(second_command).to be_called_with_arg('foo').at_position(0)
-        expect(second_command).to be_called_with_arg('bar').at_position(1)
-        expect(second_command).to_not be_called_with_arg('foo').at_position(1)
-        expect(second_command).to_not be_called_with_arg('bar').at_position(0)
-        expect(second_command).to be_called_with_arg('boo').at_position(1)
-      end
-    end
-
-    context 'with flag passed in to command' do
-      it 'matches against flagged parameter' do
-        subject
-        expect(flagged_command_stub).to be_called_with_arg('flagged_arg').with_flag('-d')
-        expect(flagged_command_stub).to_not be_called_with_arg('flagged_arg').with_flag('-no_call_flag')
       end
     end
 
