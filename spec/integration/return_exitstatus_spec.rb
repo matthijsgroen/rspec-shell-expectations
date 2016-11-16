3059169b8f038e8ee34b709168a55db057dfb492
Mike Urban <mike.david.urban@gmail.com>
2016-11-15 21:55:47 -0500

    Revert "cleanup rubocop errors from merge"

--- spec/integration/return_exitstatus_spec.rb
+++ spec/integration/return_exitstatus_spec.rb
@@ -5,6 +5,7 @@ describe 'very simple script that just exits' do
   include Rspec::Bash
   let(:stubbed_env) { create_stubbed_env }
   it 'returns an exit status of 0' do
+
     out, err, rv = stubbed_env.execute_inline('exit 0')
 
     expect(out).to eql ''
