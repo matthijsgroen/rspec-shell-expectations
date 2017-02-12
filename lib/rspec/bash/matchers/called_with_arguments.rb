372cb8e20d8f0a2cf608c64b1abd3096d505fdf0
Ben Brewer <ben@benbrewer.me>
2017-02-12 18:21:03 -0500

    remove diff from negative case, and make the comparison more clear for positive case

--- lib/rspec/bash/matchers/called_with_arguments.rb
+++ lib/rspec/bash/matchers/called_with_arguments.rb
@@ -19,12 +19,19 @@ RSpec::Matchers.define :be_called_with_arguments do |*expected_argument_list|
   end
 
   failure_message do |actual_command|
-    "expected #{actual_command.command} to be called with arguments #{expected_argument_list}"
+    command_name = actual_command.command
+
+    formatted_expected_call = "#{command_name} #{expected_argument_list.join(' ')}"
+    formatted_actual_calls = actual_command.call_log.call_log_arguments.map do |arg_array|
+      "#{command_name} #{arg_array.join(' ')}"
+    end.join("\n")
+
+    "Expected #{actual_command.command} to be called with arguments #{expected_argument_list}\n\n" \
+      "Expected Calls:\n#{formatted_expected_call}\n\n" \
+      "Actual Calls:\n#{formatted_actual_calls}\n"
   end
 
   failure_message_when_negated do |actual_command|
-    "expected #{actual_command.command} not to be called with arguments #{expected_argument_list}"
+    "Expected #{actual_command.command} not to be called with arguments #{expected_argument_list}"
   end
-
-  diffable
 end
--- spec/matchers/be_called_with_arguments_spec.rb
+++ spec/matchers/be_called_with_arguments_spec.rb
@@ -26,6 +26,34 @@ describe 'be_called_with_arguments' do
       it 'correctly matches when wildcard is used for all arguments' do
         expect(@command).to be_called_with_arguments(anything, anything)
       end
+
+      it 'displays the diff between what was called and what was expected' do
+        begin
+          expect(@command).to be_called_with_arguments('not_first_argument', 'second_argument')
+        rescue RSpec::Expectations::ExpectationNotMetError => rex
+          expected_error_string = <<-multiline_string
+Expected stubbed_command to be called with arguments ["not_first_argument", "second_argument"]
+
+Expected Calls:
+stubbed_command not_first_argument second_argument
+
+Actual Calls:
+stubbed_command first_argument second_argument
+stubbed_command third_argument fourth_argument
+multiline_string
+          expect(rex.message.uncolorize).to eql expected_error_string
+        end
+      end
+
+      it 'displays the diff between what was called and what was expected (negative case)' do
+        begin
+          expect(@command).to_not be_called_with_arguments('first_argument', 'second_argument')
+        rescue RSpec::Expectations::ExpectationNotMetError => rex
+          expected_error_string = 'Expected stubbed_command not to be ' \
+            'called with arguments ["first_argument", "second_argument"]'
+          expect(rex.message.uncolorize).to eql expected_error_string
+        end
+      end
     end
     context 'and the times chain call' do
       before(:each) do
