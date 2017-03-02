37886187bb8801ed520e52ca1ebb244789e15928
Ben Brewer <ben@benbrewer.me>
2017-03-01 19:47:14 -0500

    add the rest of the wildcard examples for output

--- spec/integration/stub_output_spec.rb
+++ spec/integration/stub_output_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-# TODO: add tests for anything, any_args, etc.
 describe 'Stub command output' do
   include Rspec::Bash
   let(:stubbed_env) { create_stubbed_env }
@@ -97,12 +96,78 @@ describe 'Stub command output' do
     end
   end
 
+  # TODO: it is a bug that these require a \n in the output
   describe 'stubbing standard-err' do
-    it 'changes standard-out' do
+    it 'changes standard-error' do
       command1_stub.outputs('world', to: :stderr)
       output, error, _status = stubbed_env.execute "#{script_path} 1>/dev/null"
       expect(error).to eql "world\n"
       expect(output).to be_empty
+    end
+    context 'when given an exact argument match' do
+      let(:error) do
+        command1_stub
+          .with_args('first_argument', 'second_argument')
+          .outputs('hello', to: :stderr)
+        _, error, = stubbed_env.execute_inline('command1 first_argument second_argument')
+        error
+      end
+
+      it 'outputs the expected error to stderr' do
+        expect(error).to eql "hello\n"
+      end
+    end
+    context 'when given an anything argument match' do
+      let(:error) do
+        command1_stub
+          .with_args('first_argument', anything)
+          .outputs('i respond to anything', to: :stderr)
+        _, error, = stubbed_env.execute_inline('command1 first_argument piglet')
+        error
+      end
+
+      it 'outputs the expected error to stderr' do
+        expect(error).to eql "i respond to anything\n"
+      end
+    end
+    context 'when given any_args argument match' do
+      let(:error) do
+        command1_stub
+          .with_args(any_args)
+          .outputs('i respond to any_args', to: :stderr)
+        _, error, = stubbed_env.execute_inline('command1 poglet piglet')
+        error
+      end
+
+      it 'outputs the expected error to stderr' do
+        expect(error).to eql "i respond to any_args\n"
+      end
+    end
+    context 'when given other types of RSpec::Mock::ArgumentMatcher argument match' do
+      let(:error) do
+        command1_stub
+          .with_args(instance_of(String), instance_of(String))
+          .outputs('i respond to instance_of', to: :stderr)
+        _, error, = stubbed_env.execute_inline('command1 poglet 1')
+        error
+      end
+
+      it 'outputs the expected error to stderr' do
+        expect(error).to eql "i respond to instance_of\n"
+      end
+    end
+    context 'when given regex argument match' do
+      let(:error) do
+        command1_stub
+          .with_args(/p.glet/, /p.glet/)
+          .outputs('i respond to regex', to: :stderr)
+        _, error, = stubbed_env.execute_inline('command1 poglet piglet')
+        error
+      end
+
+      it 'outputs the expected error to stderr' do
+        expect(error).to eql "i respond to regex\n"
+      end
     end
   end
 
@@ -120,6 +185,68 @@ describe 'Stub command output' do
       expect(error).to be_empty
       expect(output).to be_empty
       expect(Pathname.new(filename).read).to eql 'world'
+    end
+
+    let(:temp_file) { Tempfile.new('for-testing') }
+    context 'when given an exact argument match' do
+      before(:each) do
+        command1_stub
+          .with_args('first_argument', 'second_argument')
+          .outputs('hello', to: temp_file.path)
+        stubbed_env.execute_inline('command1 first_argument second_argument')
+      end
+
+      it 'outputs the expected content to the file' do
+        expect(temp_file.read).to eql 'hello'
+      end
+    end
+    context 'when given an anything argument match' do
+      before(:each) do
+        command1_stub
+          .with_args('first_argument', anything)
+          .outputs('i respond to anything', to: temp_file.path)
+        stubbed_env.execute_inline('command1 first_argument second_argument')
+      end
+
+      it 'outputs the expected content to the file' do
+        expect(temp_file.read).to eql 'i respond to anything'
+      end
+    end
+    context 'when given any_args argument match' do
+      before(:each) do
+        command1_stub
+          .with_args(any_args)
+          .outputs('i respond to any_args', to: temp_file.path)
+        stubbed_env.execute_inline('command1 poglet piglet')
+      end
+
+      it 'outputs the expected content to the file' do
+        expect(temp_file.read).to eql 'i respond to any_args'
+      end
+    end
+    context 'when given other types of RSpec::Mock::ArgumentMatcher argument match' do
+      before(:each) do
+        command1_stub
+          .with_args(instance_of(String), instance_of(String))
+          .outputs('i respond to instance_of', to: temp_file.path)
+        stubbed_env.execute_inline('command1 poglet 1')
+      end
+
+      it 'outputs the expected content to the file' do
+        expect(temp_file.read).to eql 'i respond to instance_of'
+      end
+    end
+    context 'when given regex argument match' do
+      before(:each) do
+        command1_stub
+          .with_args(/p.glet/, /p.glet/)
+          .outputs('i respond to regex', to: temp_file.path)
+        stubbed_env.execute_inline('command1 poglet piglet')
+      end
+
+      it 'outputs the expected content to the file' do
+        expect(temp_file.read).to eql 'i respond to regex'
+      end
     end
 
     describe 'using passed argument as filename' do
