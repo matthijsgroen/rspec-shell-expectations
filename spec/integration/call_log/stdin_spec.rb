568f7cbe4833d220c7caf06a766be4e459f9627d
Ben Brewer <ben@benbrewer.me>
2017-03-12 14:12:33 -0400

    add better integration tests for stdin that include wild cards, etc.

--- spec/integration/call_log/stdin_spec.rb
+++ spec/integration/call_log/stdin_spec.rb
@@ -3,35 +3,45 @@ require 'spec_helper'
 describe 'CallLog' do
   include Rspec::Bash
   let(:stubbed_env) { create_stubbed_env }
-  let!(:command1_stub) { stubbed_env.stub_command('command1') }
-
-  let(:script) do
-    <<-SCRIPT
-      echo "foo bar" | command1
-      echo "baz" | command1 'hello'
-    SCRIPT
-  end
-  let(:script_path) { Pathname.new '/tmp/test_script.sh' }
-
-  before do
-    script_path.open('w') { |f| f.puts script }
-    script_path.chmod 0777
-
-    stubbed_env.execute script_path.to_s
-  end
-
-  after do
-    script_path.delete
-  end
+  let!(:first_command) { stubbed_env.stub_command('first_command') }
 
   context '#stdin' do
-    it 'returns the stdin' do
-      expect(command1_stub.stdin).to match 'foo bar'
-    end
+    context 'when given multiple command calls' do
+      before(:each) do
+        stubbed_env.execute_inline(
+            <<-multiline_script
+              echo -n 'first_call' | first_command first_argument second_argument
+              echo -n 'second_call' | first_command first_argument second_argument third_argument
+        multiline_script
+        )
+      end
 
-    context 'with arguments' do
-      it 'returns the stdin' do
-        expect(command1_stub.with_args('hello').stdin).to match 'baz'
+      it 'matches for implied anything matches' do
+        expect(first_command.stdin).to eql 'first_call'
+      end
+
+      it 'does not match for non-matches' do
+        expect(first_command.with_args('first_argument').stdin).to be nil
+      end
+
+      it 'matches for exact matches' do
+        expect(first_command.with_args('first_argument', 'second_argument').stdin).to eql 'first_call'
+      end
+
+      it 'matches for anything matches' do
+        expect(first_command.with_args(anything, anything, 'third_argument').stdin).to eql 'second_call'
+      end
+
+      it 'matches for any_args matches' do
+        expect(first_command.with_args(any_args).stdin).to eql 'first_call'
+      end
+
+      it 'matches for other types of RSpec::Mock::ArgumentMatcher matches' do
+        expect(first_command.with_args(instance_of(String), instance_of(String)).stdin).to eql 'first_call'
+      end
+
+      it 'matches for regex matches' do
+        expect(first_command.with_args(/f..st_argument/, /se..nd_argument/, /.*/).stdin).to eql 'second_call'
       end
     end
   end
